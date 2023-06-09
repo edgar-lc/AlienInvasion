8bfd8b16abeb3496c056602dbf99526619555a70
Edgar Lule <edgrlulec21@gmail.com>
2023-06-08 17:17:15 -0700

    added so the ship can move right

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,22 +23,29 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
-            
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
     
     def _check_events(self):
         """Respond to keypresses and mouse events"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme() # Draws the ship to the screen
 
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+        
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
