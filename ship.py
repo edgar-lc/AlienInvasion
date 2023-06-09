c1ee8e539b399cbdd8fd7fde1b1c8091fd99d42a
Edgar Lule <edgrlulec21@gmail.com>
2023-06-08 21:59:52 -0700

    Adjusted ships speed

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game): # ai_game gives us access to the alien invasion object we created
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen # we want screen of ship to match up with screen of tha game
+        self.settings = ai_game.settings # We create a settings attribute for Ship, so we can use it in update()
         self.screen_rect = ai_game.screen.get_rect() # get_rect method allows 
 
         # Load the ship image and get its rect.
@@ -15,6 +16,9 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom # this is saying the images midbottom is the same as the screens midbottom, this is how we get our ship to always start in the middle of the screen
 
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
@@ -22,9 +26,12 @@ class Ship:
     def update(self):
         """Update the ship's position based on the movement flag."""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
